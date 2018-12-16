21803926324f5c43c79afc60a32fc9fcaf87781e
ArcTLK <33800818+ArcTLK@users.noreply.github.com>
2018-12-16 21:38:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-echo "Hello";
+header('Content-type: application/json');
+$update = file_get_contents('php://input');
+$update = json_decode($update, true);
 
+$chatId = $request['message']['chat']['id'];
+$messageText = $request['message']['text'];
+
+$message = ['method' => 'sendMessage', 'chat_id' => $chatId, 'text' => $message];
+echo json_encode($message);
