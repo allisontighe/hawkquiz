cda2455fdebf2832a081136c1b86e3cc0f2db178
allisontighe <allisontighe592@gmail.com>
2018-12-17 09:27:44 -0600

    tesing2

--- stuff.php
+++ stuff.php
@@ -4,7 +4,7 @@ class Game extends stuff{
     
 private $connection;
 
-public function __construct(array $message) {
+function __construct(array $message) {
     
     $this->connection = new Connection;
     
