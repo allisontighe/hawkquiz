c6bf9bd8532285027dba4951a93523b5753d4249
allisontighe <allisontighe592@gmail.com>
2018-12-17 02:43:46 -0600

    redo

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $update = file_get_contents('php://input');
 $update = json_decode($update, true);
 
 $chatId = $update['message']['chat']['id'];
-$messageText = $update;
+$messageText = $update['message']['text'];
 
 $message = ['method' => 'sendMessage', 'chat_id' => $chatId, 'text' => $messageText];
 echo json_encode($message);
